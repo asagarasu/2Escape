909c3a621fc9b6c99b9f0597747ac205feec3790
BetsyFu <zf48@cornell.edu>
2018-03-27 17:19:43 -0400

    Command

--- command.mli
+++ command.mli
@@ -1,14 +1,10 @@
 (* [command] represents a command input by a player. *)
 
 type command =
-| Go of string
+| Go of {u:bool; d:bool; l:bool; r:bool}
 | Take of string
 | Drop of string
-| Quit
-| Look
-| Inv
-| Click
-| Unknown
+
 
 (* [parse str] is the command that represents player input [str].
  * requires: [str] is one of the commands forms described in the
