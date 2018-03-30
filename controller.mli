18d82ef29d3dcb02a20d28b4de5b04c8a536bd66
Aaron Lou <al968@cornell.edu>
2018-03-29 20:27:32 -0400

    renaming

--- controller.mli
+++ controller.mli
@@ -6,4 +6,4 @@ val mouseclick_down : Dom_html.mouseEvent Js.t -> bool Js.t
 
 val mouseclick_up : Dom_html.mouse_button Js.t -> bool Js.t
 
-val play_game : Dom_html.canvasRenderingContext2D Js.t -> unit()
+val play_game : Dom_html.canvasRenderingContext2D Js.t -> unit
