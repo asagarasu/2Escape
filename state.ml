72e9636eb46f7939adac7e18bd94c92bbb085fc9
Anneliese Lu <yl668@cornell.edu>
2018-04-15 17:01:21 -0400

    help remove

--- state.ml
+++ state.ml
@@ -1,13 +1,5 @@
 open Types
 
-
-type value =
-  | Row_col_rooms  of (int*int*room)list
-  | Map_matrix of room
-  | Inventory of string
-  | Chat of textMessage
-
-
 let do_command c t =
   failwith "Unimplemented"
 
