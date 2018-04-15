dfd5a0dbea714694fc5f263bd3c57991489dd85a
Anneliese Lu <yl668@cornell.edu>
2018-04-15 16:33:51 -0400

    state spec init

--- state.mli
+++ state.mli
@@ -1,5 +1,20 @@
 open Types
 
+(* [state] is the type serving as the main game state. It can only
+   be changed after verfying the commands received from the
+   clients.
+
+   [state] contains values:
+   * row_col_rooms (room_id)
+   * map_matrix of the rooms
+   * location of characters/objects
+   * inventory
+   * chart
+
+   [state] has functions:
+   * update_state
+   * diff : state -> command -> command -> log
+*)
 type t
 
 val do_command: Command.command -> t -> t
