889cd68ddc8e913f56fd0aed45eb1f438dc0f976
heatherzheng <sz488@cornell.edu>
2018-05-04 17:49:01 -0400

    rewrite socket

--- server.ml
+++ server.ml
@@ -1,48 +1,53 @@
 open Unix
 open Lwt
+open Lwt_io
+
+let counter = ref 0
 
 let () = Lwt_log.add_rule "*" Lwt_log.Info
 
 (* type of server. input channels and outputchannel *)
 type t = in_channel list * out_channel
 
-let create_server (port : int) : unit = 
-  failwith "TODO"
-
-(** 
- * Temporary Helper method to get the ip address of the local computer
- * Used for testing client-server interaction
- *)
+(**get the ip address of the local computer*)
 let get_my_addr () =
   (Unix.gethostbyname(Unix.gethostname())).Unix.h_addr_list.(0) ;;
 
-(**
- * Temporary method to test trivial interactions with server
- * send back the same command received from the client
- *)
-let print_out ic oc =
-   try while true do
-         let s = input_line ic in
-          output_string oc (s^"\n") ; flush oc
-       done
-   with _ -> Printf.printf "End of text\n" ;
-              flush Pervasives.stdout;
-               exit 0 ;;
+let handle_message msg =
+    match msg with
+    | "read" -> string_of_int !counter
+    | "inc"  -> counter := !counter + 1; "Counter has been incremented"
+    | _      -> "Unknown command"
 
-(**
- * Temporary method to test server instantiation
- *)			   
-let main_server serv_fun =
-   try
-   let port = 40005 in
-   let addr = ADDR_INET (get_my_addr (), port)
-   in establish_server print_out addr
-       with
-         Failure _ ->
-         Printf.eprintf "serv_up : bad port number\n" ;;
+let rec handle_connection ic oc () =
+    Lwt_io.read_line_opt ic >>=
+    (fun msg ->
+        match msg with
+        | Some msg ->
+            let reply = handle_message msg in
+            Lwt_io.write_line oc reply >>= handle_connection ic oc
+        | None -> Lwt_log.info "Connection closed" >>= return)
 
-(**
- * Temporary method to start server
- *)
-let start_server () =
-   Unix.handle_unix_error main_server print_out;;
+let accept_connection conn =
+    let fd, _ = conn in
+    let ic = Lwt_io.of_fd Lwt_io.Input fd in
+    let oc = Lwt_io.of_fd Lwt_io.Output fd in
+    Lwt.on_failure (handle_connection ic oc ()) (fun e -> Lwt_log.ign_error (Printexc.to_string e));
+    Lwt_log.info "New connection" >>= return
+
+let create_socket (port : int) =
+    let open Lwt_unix in
+    let sock = socket PF_INET SOCK_STREAM 0 in
+    bind sock @@ ADDR_INET (get_my_addr (), port);
+    listen sock 10;
+    sock
+
+let create_server_help sock =
+    let rec serve () =
+        Lwt_unix.accept sock >>= accept_connection >>= serve
+    in serve
+
+let create_server (port : int) : unit =
+    let sock = create_socket port in
+    let serve = create_server_help sock in
+    Lwt_main.run @@ serve ()
