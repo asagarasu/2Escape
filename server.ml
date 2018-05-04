6f0a364b670a620fdec0d1466cd32dd1dc34bac8
heatherzheng <sz488@cornell.edu>
2018-05-04 18:08:57 -0400

    server test works

--- server.ml
+++ server.ml
@@ -13,12 +13,15 @@ type t = in_channel list * out_channel
 let get_my_addr () =
   (Unix.gethostbyname(Unix.gethostname())).Unix.h_addr_list.(0) ;;
 
+(**temporary function try to read the msg and use handle_connection 
+ *to do some simple calculation*)
 let handle_message msg =
     match msg with
     | "read" -> string_of_int !counter
     | "inc"  -> counter := !counter + 1; "Counter has been incremented"
     | _      -> "Unknown command"
 
+(**process input and output*)	
 let rec handle_connection ic oc () =
     Lwt_io.read_line_opt ic >>=
     (fun msg ->
@@ -29,12 +32,13 @@ let rec handle_connection ic oc () =
         | None -> Lwt_log.info "Connection closed" >>= return)
 
 let accept_connection conn =
-    let fd, _ = conn in
+    let fd, sockaddr = conn in (**sockaddr need to be used in the handle message later*)
     let ic = Lwt_io.of_fd Lwt_io.Input fd in
     let oc = Lwt_io.of_fd Lwt_io.Output fd in
     Lwt.on_failure (handle_connection ic oc ()) (fun e -> Lwt_log.ign_error (Printexc.to_string e));
     Lwt_log.info "New connection" >>= return
 
+(**create the socket*)
 let create_socket (port : int) =
     let open Lwt_unix in
     let sock = socket PF_INET SOCK_STREAM 0 in
@@ -42,6 +46,7 @@ let create_socket (port : int) =
     listen sock 10;
     sock
 
+(**read the message and send out*)
 let create_server_help sock =
     let rec serve () =
         Lwt_unix.accept sock >>= accept_connection >>= serve
